8e516dbb67a87e545ba45e52c410c62ec41ef2fe
936656230 <yuchanglinlin@163.com>
2016-08-31 17:49:48 +0800

    提交代码

---
+++ MessageUIDemo/MessageUIDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MessageUIDemo
+//
+//  Created by qunqu on 16/8/31.
+//  Copyright © 2016年 YuChanglin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MessageUIDemo/MessageUIDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MessageUIDemo
+//
+//  Created by qunqu on 16/8/31.
+//  Copyright © 2016年 YuChanglin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
